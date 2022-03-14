1ce88bc12ac74d54e223d1a92f77b23a4eaeee9c
hyeop <dwby.jr@gmail.com>
2022-03-14 11:13:36 +0900

    version 1.3.3

--- app_v2/internal/database.py
+++ app_v2/internal/database.py
@@ -21,5 +21,11 @@ def initialize_db() -> ServiceResource:
         #  aws_secret_access_key= Config.DB_SECRET_ACCESS_KEY)
           region_name='ap',
           aws_access_key_id= 'x',
+          aws_secret_access_key= 'x'),
+
+    s3= boto3.resource('s3',
+          region_name='ap',
+          aws_access_key_id= 'x',
           aws_secret_access_key= 'x')
-    return dynamodb
+
+    return dynamodb, s3
