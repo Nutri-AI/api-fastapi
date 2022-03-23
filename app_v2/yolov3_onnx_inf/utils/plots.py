7dbc1151f62bb2b8c756cc6115ee6521056e42af
hyeniii <hyewonee96@gmail.com>
2022-03-23 11:10:55 +0900

    changed plots.py scale

--- app_v2/yolov3_onnx_inf/utils/plots.py
+++ app_v2/yolov3_onnx_inf/utils/plots.py
@@ -65,7 +65,7 @@ class Annotator:
     #  Annotator for train/val mosaics and jpgs and detect/hub inference annotations
     def __init__(self, im, line_width=None, font_size=None, font='Arial.ttf', pil=False, example='abc'):
         assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to Annotator() input images.'
-        self.pil = pil or not is_ascii(example) or is_chinese(example)
+        self.pil = pil or not is_ascii(example)
         if self.pil:  # use PIL
             self.im = im if isinstance(im, Image.Image) else Image.fromarray(im)
             self.draw = ImageDraw.Draw(self.im)
